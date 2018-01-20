a45ce2d9c7f3ec2406530ff7b3a3f540d1443802
Blake Oliver <oliver22213@me.com>
2018-01-20 12:00:54 -0500

    Actually make utils::get_ignores only recurse once if multiple ignore files exist; this was supposed to be the case several commits previously, but I didn't completely fix this

--- src/utils.rs
+++ src/utils.rs
@@ -16,20 +16,20 @@ pub fn ignored_files (path: &Path, ignore_files: Vec<File>) -> BTreeSet<String> 
     // this has to walk `path` and check every file
     let mut ignores: BTreeSet<String> = BTreeSet::new();
     let mut walker = WalkDir::new(path).into_iter();
-    for f in ignore_files {
-        while let Some(e) = walker.next() {
-            match e {
-                Ok(entry) => {
-                    // I don't do anything special if the entry is a directory, as checksums will skip it if provided
-                    // check to see if the path is excluded
+    while let Some(e) = walker.next() {
+        match e {
+            Ok(entry) => {
+                // I don't do anything special if the entry is a directory, as checksums will skip it if provided
+                // check to see if the path is excluded for each ignore file we were given
+                for f in &ignore_files {
                     let ignored_entry = f.is_excluded(entry.path()).unwrap();
                     if ignored_entry && entry.path()!= path {
                         ignores.insert(relative_name(path, entry.path()));
                     }
-                },
-                Err(e) => panic!("Error traversing directory: {}", e.description()),
-            }
+                }
+            },
+            Err(e) => panic!("Error traversing directory: {}", e.description()),
         }
-    }
+    }    
     ignores
 }
