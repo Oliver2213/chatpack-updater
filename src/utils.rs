d6467b37863b905821f2334cca401e291df2d3df
Blake Oliver <oliver22213@me.com>
2018-03-13 20:29:45 -0400

    Fix horrible recursion bugs with the gitignore library I'm using; determining files the updater should ignore takes much much less time now, but it's still not where I'd like it to be

--- src/utils.rs
+++ src/utils.rs
@@ -15,19 +15,41 @@ pub fn ignored_files (path: &Path, ignore_files: Vec<File>) -> BTreeSet<String> 
     // This function returns every file (as a string relative to the `path` arg)
     // this is for use with checksums, since it only allows you to provide a BTreeSet of strings
     // this has to walk `path` and check every file
+    /*
+This is really inefficient and I hate it, but...
+gitignore has an included_files method, which is_excluded calls every time. So I can't use is_excluded, because that would trigger a recursion on every call.
+So instead we call included_files, and check if each path I'm iterating over is in that list, which is what is_excluded does anyway; I'm just not needlessly recursing.
+    */
     let mut ignores: BTreeSet<String> = BTreeSet::new();
-    let mut walker = WalkDir::new(path).into_iter();
+    let mut included_files = vec![];
+    for f in &ignore_files {
+        let r = f.included_files().unwrap();
+        included_files.extend(r);
+        included_files.sort();
+    }
+    // now remove duplicate paths from the vec
+    included_files.dedup();
+    let mut walker = WalkDir::new(path).max_open(15).into_iter();
     while let Some(e) = walker.next() {
         match e {
             Ok(entry) => {
-                // I don't do anything special if the entry is a directory, as checksums will skip it if provided
+//                println!("Working on '{:?}'", entry.file_name());
                 // check to see if the path is excluded for each ignore file we were given
-                for f in &ignore_files {
-                    let ignored_entry = f.is_excluded(entry.path()).unwrap();
-                    if ignored_entry && entry.path()!= path {
-                        ignores.insert(relative_name(path, entry.path()));
+                if !included_files.contains(&entry.path().to_path_buf()) && entry.path() != path{
+                    ignores.insert(relative_name(path, entry.path()));
+                    // if this entry is  a directory, we can skip recursing through it, as gitignore says there's no included files here
+                    if entry.file_type().is_dir() {
+                        walker.skip_current_dir();
                     }
                 }
+/*
+                    for f in &ignore_files {
+                        let ignored_entry = f.is_excluded(entry.path()).unwrap();
+                        if ignored_entry && entry.path() != path {
+                            ignores.insert(relative_name(path, entry.path()));
+                        }
+                    }
+*/
             },
             Err(e) => panic!("Error traversing directory: {}", e.description()),
         }
