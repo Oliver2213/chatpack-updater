c6def3a23b8cf93ad357a55c544dcc557cc71b27
Blake Oliver <oliver22213@me.com>
2018-03-15 14:37:50 -0400

    Make the manifest updater bump the version before generating the manifest

--- src/bin/update-manifest.rs
+++ src/bin/update-manifest.rs
@@ -37,20 +37,70 @@ fn main () {
         println!("The '{}' directory doesn't exist; unable to create / update manifest.", TARGET_DIR);
         return;
     }
-    let mut cp_manifest_path: PathBuf = env::current_dir().unwrap();
-    cp_manifest_path.push(&MANIFEST_FILENAME);
-    // check if there's an existing hash manifest
-    if cp_manifest_path.exists() == false {
-        println!("Creating initial {} manifest...", TARGET_DIR);
-    } else {
-        println!("Rebuilding {}'s manifest...", TARGET_DIR);
-    }
+    // update the version        
+    // do this first so the generated file gets added to the hash manifest
+    // since the lines below are in different scopes (and thus variables defined won't be valid outside of them), I declare the ones I want to keep here
     // clone cp_path (so it's a new object, rather than taking ownership) so cp_version_path can add to it
     let mut cp_version_path = cp_path.clone();
     cp_version_path.push(VERSION_FILENAME);
     // Check to see if there's an existing version file
     if cp_version_path.exists() == false {
         println!("{}'s version file not found; one will be created.", TARGET_DIR);
+    }
+    let mut version_file;
+    let mut version;
+    // if a version file doesn't exist, create one and populate it with the current version (based on the date)
+    // Otherwise, read what does exist, convert it to a `Version`, and .update() it
+    if cp_version_path.exists() == false {
+        version_file = match File::create(&cp_version_path) {
+            Err(why) => panic!("couldn't create {}: {}", cp_version_path.display(), why.description()),
+            Ok(file) => file,
+        };
+        // get current date-based version, for use further down
+        version = Version::new();
+    } else {
+        // the version file exists, so we need to read it into a string, convert that to a `Version`, run .update(), and then seek to the start of the file so it can be written out
+        version_file = match OpenOptions::new().read(true).write(true).open(&cp_version_path) {
+            Ok(mut file) => {
+                let mut str_ver: String = String::new();
+                // now read all the bytes in the opened file into a string
+                match file.read_to_string(&mut str_ver) {
+                    Ok(_) => (),
+                    Err(why) => panic!("Can't read version file {}: {}", cp_version_path.display(), why.description()),
+                }
+                str_ver = str_ver.trim().into();
+                if str_ver.is_empty() {
+                    // file is empty for some reason; use the current date-based version
+                    println!("Warning: the version file is empty; using a new version string because an old one doesn't exist to update.");
+                    version = Version::new();
+                } else {
+                    version = Version::from_string(&str_ver);
+                    version.update();
+                }
+                // delete the current contents of the file so it can be written to later with just the newly-created version as a string
+                file.seek(SeekFrom::Start(0)).expect("Can't seek to offset 0 in the version file to overwrite it.");
+                match file.set_len(0){
+                    Ok(_) => (),
+                    Err(why) => panic!("Can't overwrite {}: {}", cp_version_path.display(), why.description()),
+                }
+                // return version_file
+                file
+            },
+            Err(why) => panic!("Can't read from {}: {}", cp_version_path.display(), why.description()),
+        };
+    } // end the exists if block
+    match version_file.write_all(version.to_string().as_bytes()) {
+        Ok(_) => version_file.flush().expect("Can't write version out to file."),
+        Err(why) => panic!("Can't write new version to {}: {}", cp_version_path.display(), why.description()),
+    }
+    
+    // check if there's an existing hash manifest
+    let mut cp_manifest_path: PathBuf = env::current_dir().unwrap();
+    cp_manifest_path.push(&MANIFEST_FILENAME);
+    if cp_manifest_path.exists() == false {
+        println!("Creating initial {} manifest...", TARGET_DIR);
+    } else {
+        println!("Rebuilding {}'s manifest...", TARGET_DIR);
     }
     let mut ignores: BTreeSet<String> = BTreeSet::new();
     let mut standard_ignores_path = cp_path.clone();
@@ -97,55 +147,7 @@ fn main () {
         }, // end of the ok block
         Err(why) => panic!("couldn't create or open {}: {}", cp_manifest_path.display(), why.description()),
     } // end of the manifest file open error checking block
-    //now update the version        
-    // since the lines below are in different scopes (and thus variables defined won't be valid outside of them), I declare the ones I want to keep here
-    let mut version_file;
-    let mut version;
-    // if a version file doesn't exist, create one and populate it with the current version (based on the date)
-    // Otherwise, read what does exist, convert it to a `Version`, and .update() it
-    if cp_version_path.exists() == false {
-        version_file = match File::create(&cp_version_path) {
-            Err(why) => panic!("couldn't create {}: {}", cp_version_path.display(), why.description()),
-            Ok(file) => file,
-        };
-        // get current date-based version, for use further down
-        version = Version::new();
-    } else {
-        // the version file exists, so we need to read it into a string, convert that to a `Version`, run .update(), and then seek to the start of the file so it can be written out
-        version_file = match OpenOptions::new().read(true).write(true).open(&cp_version_path) {
-            Ok(mut file) => {
-                let mut str_ver: String = String::new();
-                // now read all the bytes in the opened file into a string
-                match file.read_to_string(&mut str_ver) {
-                    Ok(_) => (),
-                    Err(why) => panic!("Can't read version file {}: {}", cp_version_path.display(), why.description()),
-                }
-                str_ver = str_ver.trim().into();
-                if str_ver.is_empty() {
-                    // file is empty for some reason; use the current date-based version
-                    println!("Warning: the version file is empty; using a new version string because an old one doesn't exist to update.");
-                    version = Version::new();
-                } else {
-                    version = Version::from_string(&str_ver);
-                    version.update();
-                }
-                // delete the current contents of the file so it can be written to later with just the newly-created version as a string
-                file.seek(SeekFrom::Start(0)).expect("Can't seek to offset 0 in the version file to overwrite it.");
-                match file.set_len(0){
-                    Ok(_) => (),
-                    Err(why) => panic!("Can't overwrite {}: {}", cp_version_path.display(), why.description()),
-                }
-                // return version_file
-                file
-            },
-            Err(why) => panic!("Can't write to {}: {}", cp_version_path.display(), why.description()),
-        };
-    } // end the exists if block
-    match version_file.write_all(version.to_string().as_bytes()) {
-        Ok(_) => version_file.flush().expect("Can't write version out to file."),
-        Err(why) => println!("Can't write new version to {}: {}", cp_manifest_path.display(), why.description()),
-    }
-
+    
     // We've now created then written a manifest out to disk, then updated the version
     // Now check to see if this program is being used as a git hook, and if so, add the manifest and version files to git's index (so they automatically get included in commits)
     match env::current_exe() {
